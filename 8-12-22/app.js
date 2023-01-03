6086d52fc19c0471db2559b1506190733bc9fb02
Thrushitha-25 <75431446+Thrushitha-25@users.noreply.github.com>
2023-01-03 18:03:11 +0530

    Add files via upload

---
+++ 8-12-22/app.js
@@ -0,0 +1,39 @@
+const express=require('express');
+const path=require('path');
+const mysql=require('mysql');
+const dotenv=require('dotenv');
+
+dotenv.config({path:'./.env'})
+
+const app=express();
+
+
+const db=mysql.createConnection({
+    host:process.env.DATABASE_HOST,
+    user:process.env.DATABASE_USER,
+    password:process.env.DATABASE_PASSWORD,
+    database:process.env.DATABASE
+});
+
+const publicDirectory=path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+
+app.set('view engine','hbs');
+
+db.connect( (error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Mysql connected..")
+    }
+});
+
+
+app.get("/",(req,res)=>{
+    //res.send("<hi>Home Page</h1>")
+    res.render("index");
+});
+
+app.listen(8000,()=>{
+    console.log("Server Running on posr 8000");
+})
